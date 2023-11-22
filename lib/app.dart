c8ca89fadb4f708018c4ee9097d50909cdd3502d
Waldo Sobrino <waldo2810@gmail.com>
2023-11-22 17:41:24 -0500

    improve home appbar

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:movies/views/home.dart';
-// import 'package:movies/views/search.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -9,31 +8,15 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int _selectedIndex = 0;
-  static const List<Widget> _screenOptions = [
-    Home(),
-    // SearchScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Movies')),
-      body: Container(child: _screenOptions[_selectedIndex]),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: const <BottomNavigationBarItem>[
-      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-      //     BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search')
-      //   ],
-      //   currentIndex: _selectedIndex,
-      //   onTap: _onItemTapped,
-      // ),
+      appBar: AppBar(
+        title: const Text('Movies',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.black,
+      ),
+      body: const Home(),
     );
   }
 }
